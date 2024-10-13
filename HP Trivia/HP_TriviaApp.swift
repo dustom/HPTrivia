91b630237898ee4a63e4e4032cdf70a83d34be18
dustom <dusek.tomas@icloud.com>
2024-10-13 09:01:32 +0200

    Initial Commit

---
+++ HP Trivia/HP_TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HP_TriviaApp.swift
+//  HP Trivia
+//
+//  Created by Tomáš Dušek on 13.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct HP_TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
